99251ca401f180d99e1a221891cb5ef6ad8a8db0
Luan Panno <luanpanno@gmail.com>
2021-02-07 01:10:11 -0300

    refactor: move protocols and helpers to an index

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,11 @@
 import { InvalidParamError, MissingParamError } from '../errors';
-import { badRequest, serverError } from '../helpers/httpHelper';
-import { Controller } from '../protocols/controller';
-import { EmailValidator } from '../protocols/emailValidator';
-import { HttpRequest, HttpResponse } from '../protocols/http';
+import { badRequest, serverError } from '../helpers';
+import {
+  Controller,
+  EmailValidator,
+  HttpRequest,
+  HttpResponse,
+} from '../protocols';
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
