d479e784cb620fbabcf788d5510a387ee9878ced
Luan Panno <luanpanno@gmail.com>
2021-02-10 18:27:28 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/SignUp/SignUp.spec.ts
+++ src/presentation/controllers/SignUp/SignUp.spec.ts
@@ -35,7 +35,7 @@ const makeAddAccount = (): IAddAccount => {
       const fakeAccount = {
         id: 'id',
         name: 'name',
-        email: 'any_email@email.com',
+        email: 'valid_email@email.com',
         password: 'password',
       };
       return fakeAccount;
@@ -240,5 +240,28 @@ describe('SignUp Controller', () => {
 
     expect(httpResponse.statusCode).toBe(500);
     expect(httpResponse.body).toEqual(new ServerError());
+  });
+
+  test('should return 200 if a valid data is provided', () => {
+    const { sut } = makeSut();
+
+    const httpRequest = {
+      body: {
+        name: 'name',
+        email: 'valid_email@email.com',
+        password: 'password',
+        passwordConfirmation: 'password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(200);
+    expect(httpResponse.body).toEqual({
+      id: 'id',
+      name: 'name',
+      email: 'valid_email@email.com',
+      password: 'password',
+    });
   });
 });
