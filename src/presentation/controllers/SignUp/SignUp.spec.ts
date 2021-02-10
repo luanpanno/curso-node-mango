4a80b15391c9a535fc7eba7d1cb899e14aeba67c
Luan Panno <luanpanno@gmail.com>
2021-02-10 18:20:08 -0300

    test: ensure SignUpController throws error if AddAccount fails

--- src/presentation/controllers/SignUp/SignUp.spec.ts
+++ src/presentation/controllers/SignUp/SignUp.spec.ts
@@ -220,4 +220,25 @@ describe('SignUp Controller', () => {
       password: 'password',
     });
   });
+
+  test('should return 500 if AddAccount throws', () => {
+    const { sut, addAccountStub } = makeSut();
+    jest.spyOn(addAccountStub, 'add').mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const httpRequest = {
+      body: {
+        name: 'name',
+        email: 'any_email@email.com',
+        password: 'password',
+        passwordConfirmation: 'password',
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(500);
+    expect(httpResponse.body).toEqual(new ServerError());
+  });
 });
