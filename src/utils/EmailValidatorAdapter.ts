8e3bf1c4c773533a5660d8f4d11f3a3c3a6fb64b
Luan Panno <luanpanno@gmail.com>
2021-02-10 21:15:36 -0300

    test: ensure EmailValidator returns true if validator returns true

--- src/utils/EmailValidatorAdapter.ts
+++ src/utils/EmailValidatorAdapter.ts
@@ -1,8 +1,8 @@
 import { IEmailValidator } from '../presentation/protocols/IEmailValidator';
+import validator from 'validator';
 
 export class EmailValidatorAdapter implements IEmailValidator {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   isValid(email: string): boolean {
-    return false;
+    return validator.isEmail(email);
   }
 }
