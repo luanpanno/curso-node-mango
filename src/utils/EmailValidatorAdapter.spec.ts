5773157019680f75094ed0944b915a9db86aa403
Luan Panno <luanpanno@gmail.com>
2021-02-10 21:18:33 -0300

    test: ensure validator is called with correct email

--- src/utils/EmailValidatorAdapter.spec.ts
+++ src/utils/EmailValidatorAdapter.spec.ts
@@ -20,9 +20,18 @@ describe('EmailValidator Adapter', () => {
 
   test('should return true if validator returns true', () => {
     const sut = new EmailValidatorAdapter();
-
     const isEmailValid = sut.isValid('valid@email.com');
 
     expect(isEmailValid).toBe(true);
+  });
+
+  test('should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter();
+    const isEmailSpy = jest.spyOn(validator, 'isEmail');
+    const email = 'any@email.com';
+
+    sut.isValid(email);
+
+    expect(isEmailSpy).toHaveBeenCalledWith(email);
   });
 });
