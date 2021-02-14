770f933fefff5f4b20309e9cc13ab56f87a24019
Luan Panno <luanpanno@gmail.com>
2021-02-14 02:50:22 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/criptography/BcryptAdapter.spec.ts
+++ src/infra/criptography/BcryptAdapter.spec.ts
@@ -1,6 +1,12 @@
 import bcrypt from 'bcrypt';
 import BcryptAdapter from './BcryptAdapter';
 
+jest.mock('bcrypt', () => ({
+  async hash(): Promise<string> {
+    return Promise.resolve('hash');
+  },
+}));
+
 describe('BcryptAdapter', () => {
   test('should call bcrypt with correct value', async () => {
     const salt = 12;
@@ -10,5 +16,14 @@ describe('BcryptAdapter', () => {
     await sut.encrypt('any_value');
 
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt);
+  });
+
+  test('should return a hash on success', async () => {
+    const salt = 12;
+    const sut = new BcryptAdapter(salt);
+
+    const hash = await sut.encrypt('any_value');
+
+    expect(hash).toBe('hash');
   });
 });
--- src/infra/criptography/BcryptAdapter.ts
+++ src/infra/criptography/BcryptAdapter.ts
@@ -8,8 +8,8 @@ class BcryptAdapter implements IEncrypter {
   }
 
   async encrypt(value: string): Promise<string> {
-    await bcrypt.hash(value, this.salt);
-    return '';
+    const hash = await bcrypt.hash(value, this.salt);
+    return hash;
   }
 }
 
