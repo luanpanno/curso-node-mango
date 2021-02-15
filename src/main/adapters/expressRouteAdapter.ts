0bbdeb45d66b1275de02addfa0965f94f46862e7
Luan Panno <luanpanno@gmail.com>
2021-02-15 03:19:07 -0300

    feat: add ExpressRouteAdapter

---
+++ src/main/adapters/expressRouteAdapter.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from 'express';
+import { IController, IHttpRequest } from '../../presentation/protocols';
+
+export const adaptRoute = (controller: IController) => {
+  return async (req: Request, res: Response) => {
+    const httpRequest: IHttpRequest = {
+      body: req.body,
+    };
+
+    const httpResponse = await controller.handle(httpRequest);
+    res.status(httpResponse.statusCode).json(httpResponse.body);
+  };
+};
