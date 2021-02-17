1ef6d902036d0c9589a8f14de9e398d1ce2cd356
Luan Panno <luanpanno@gmail.com>
2021-02-17 20:25:02 -0300

    feat: ensure SignupController returns 400 if validation fails

--- src/presentation/controllers/SignUp/SignUp.spec.ts
+++ src/presentation/controllers/SignUp/SignUp.spec.ts
@@ -252,4 +252,19 @@ describe('SignUp Controller', () => {
 
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
   });
+
+  test('should return 400 if Validation returns an error', async () => {
+    const { sut, validationStub } = makeSut();
+    const httpRequest = makeFakeRequest();
+
+    jest
+      .spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new MissingParamError('any_field'));
+
+    const httpResponse = await sut.handle(httpRequest);
+
+    expect(httpResponse).toEqual(
+      badRequest(new MissingParamError('any_field'))
+    );
+  });
 });
--- src/presentation/controllers/SignUp/SignUp.ts
+++ src/presentation/controllers/SignUp/SignUp.ts
@@ -26,7 +26,12 @@ export class SignUpController implements IController {
 
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
     try {
-      this.validation.validate(httpRequest.body);
+      const error = this.validation.validate(httpRequest.body);
+
+      if (error) {
+        return badRequest(error);
+      }
+
       const requiredFields = [
         'name',
         'email',
