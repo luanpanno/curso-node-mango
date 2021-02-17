d6a8cbd478be1c37c8c5f4e4ea0decb98bc4125f
Luan Panno <luanpanno@gmail.com>
2021-02-16 21:31:00 -0300

    test: ensure LoginController returns 500 if authentication throws

--- src/presentation/controllers/Login/Login.spec.ts
+++ src/presentation/controllers/Login/Login.spec.ts
@@ -133,4 +133,16 @@ describe('Login Controller', () => {
 
     expect(httpResponse).toEqual(unauthorized());
   });
+
+  test('should return 500 if Authentication throws', async () => {
+    const { sut, authenticationStub } = makeSut();
+
+    jest
+      .spyOn(authenticationStub, 'auth')
+      .mockReturnValueOnce(Promise.reject(new Error()));
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+
+    expect(httpResponse).toEqual(serverError(new Error()));
+  });
 });
