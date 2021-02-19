73f13e88c62837dd1efbebd9553d0a0555ad462d
Luan Panno <luanpanno@gmail.com>
2021-02-19 00:22:55 -0300

    test: ensure DbAuthentication returns null if HashComparer returns null

--- src/data/usecases/authentication/DbAuthentication.spec.ts
+++ src/data/usecases/authentication/DbAuthentication.spec.ts
@@ -118,4 +118,15 @@ describe('DbAuthentication', () => {
 
     await expect(promise).rejects.toThrow();
   });
+
+  test('should return null if HashComparer returns false', async () => {
+    const { sut, hashComparerStub } = makeSut();
+    jest
+      .spyOn(hashComparerStub, 'compare')
+      .mockReturnValueOnce(Promise.resolve(false));
+
+    const accessToken = await sut.auth(makeFakeAuthentication());
+
+    expect(accessToken).toBeNull();
+  });
 });
