6c5f9822995c6b61df4c53fa8a5be9c5e747a50c
Luan Panno <luanpanno@gmail.com>
2021-02-21 05:07:29 -0300

    feat: add login route

--- src/main/routes/loginRoutes.test.ts
+++ src/main/routes/loginRoutes.test.ts
@@ -1,6 +1,10 @@
+import { Collection } from 'mongodb';
 import request from 'supertest';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/MongoHelper';
 import app from '../config/app';
+import bcrypt from 'bcrypt';
+
+let accountCollection: Collection;
 
 describe('Login Routes', () => {
   beforeAll(async () => {
@@ -12,7 +16,7 @@ describe('Login Routes', () => {
   });
 
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts');
+    accountCollection = await MongoHelper.getCollection('accounts');
 
     await accountCollection.deleteMany({});
   });
@@ -20,12 +24,32 @@ describe('Login Routes', () => {
   describe('POST /signup', () => {
     test('should return status 200 on signup', async () => {
       await request(app)
-        .post('/api/signup')
+        .post('/api/v1/signup')
         .send({
           name: 'Luan',
           email: 'luanpanno@gmail.com',
           password: '123',
           passwordConfirmation: '123',
+        })
+        .expect(200);
+    });
+  });
+
+  describe('POST /login', () => {
+    test('should return status 200 on login', async () => {
+      const password = await bcrypt.hash('123', 12);
+
+      await accountCollection.insertOne({
+        name: 'Luan',
+        email: 'luanpanno@gmail.com',
+        password,
+      });
+
+      await request(app)
+        .post('/api/v1/login')
+        .send({
+          email: 'luanpanno@gmail.com',
+          password: '123',
         })
         .expect(200);
     });
--- src/main/routes/loginRoutes.ts
+++ src/main/routes/loginRoutes.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import { adaptRoute } from '../adapters/express/expressRouteAdapter';
+import { makeLoginController } from '../factories/login/login';
 import { makeSignupController } from '../factories/signup/signup';
 
 export default (router: Router): void => {
   router.post('/signup', adaptRoute(makeSignupController()));
+  router.post('/login', adaptRoute(makeLoginController()));
 };
