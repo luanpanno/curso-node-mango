33356af0e7682f9b9a4e01fa683fed907cc57df4
Luan Panno <luanpanno@gmail.com>
2021-02-21 03:43:05 -0300

    feat: ensure AccountMongoRepository update the account accessToken on updateAccessToken success

--- src/infra/db/mongodb/AccountRepository/AccountMongoRepository.ts
+++ src/infra/db/mongodb/AccountRepository/AccountMongoRepository.ts
@@ -1,12 +1,16 @@
 /* eslint-disable @typescript-eslint/indent */
 import { IAddAccountRepository } from '../../../../data/protocols/db/IAddAccountRepository';
 import { ILoadAccountByEmailRepository } from '../../../../data/protocols/db/ILoadAccountByEmailRepository';
+import { IUpdateAccessTokenRepository } from '../../../../data/protocols/db/IUpdateAccessTokenRepository';
 import { IAccountModel } from '../../../../domain/models/IAccount';
 import { IAddAccountModel } from '../../../../domain/usecases/IAddAccount';
 import { MongoHelper } from '../helpers/MongoHelper';
 
 class AccountMongoRepository
-  implements IAddAccountRepository, ILoadAccountByEmailRepository {
+  implements
+    IAddAccountRepository,
+    ILoadAccountByEmailRepository,
+    IUpdateAccessTokenRepository {
   async add(accountData: IAddAccountModel): Promise<IAccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts');
     const result = await accountCollection.insertOne(accountData);
@@ -19,6 +23,21 @@ class AccountMongoRepository
     const account = await accountCollection.findOne({ email });
 
     return account && MongoHelper.map(account);
+  }
+
+  async updateAccessToken(id: string, token: string): Promise<void> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+
+    await accountCollection.updateOne(
+      {
+        _id: id,
+      },
+      {
+        $set: {
+          accessToken: token,
+        },
+      }
+    );
   }
 }
 
