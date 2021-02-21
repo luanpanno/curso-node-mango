34d331d5de5f37a9893428afb5b5cabcd38d6d39
Luan Panno <luanpanno@gmail.com>
2021-02-21 03:17:47 -0300

    test: ensure JwtAdapter throws if sign throws

--- src/infra/criptography/JwtAdapter/JwtAdapter.spec.ts
+++ src/infra/criptography/JwtAdapter/JwtAdapter.spec.ts
@@ -35,4 +35,16 @@ describe('JWT Adapter', () => {
 
     expect(accessToken).toBe('any_token');
   });
+
+  test('should throw if sign throws', async () => {
+    const { sut } = makeSut();
+
+    jest.spyOn(jwt, 'sign').mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    const promise = sut.encrypt('any_id');
+
+    await expect(promise).rejects.toThrow();
+  });
 });
