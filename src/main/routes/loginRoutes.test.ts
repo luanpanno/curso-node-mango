5f94af8b47544023d26e9465a41c8db65997a851
Luan Panno <luanpanno@gmail.com>
2021-02-21 04:54:11 -0300

    refactor: rename signup routes to login routes

--- src/main/routes/loginRoutes.test.ts
+++ src/main/routes/loginRoutes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { MongoHelper } from '../../infra/db/mongodb/helpers/MongoHelper';
 import app from '../config/app';
 
-describe('Signup Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     await MongoHelper.connect(process.env.MONGO_URL);
   });
@@ -17,15 +17,17 @@ describe('Signup Routes', () => {
     await accountCollection.deleteMany({});
   });
 
-  test('should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Luan',
-        email: 'luanpanno@gmail.com',
-        password: '123',
-        passwordConfirmation: '123',
-      })
-      .expect(200);
+  describe('POST /signup', () => {
+    test('should return status 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Luan',
+          email: 'luanpanno@gmail.com',
+          password: '123',
+          passwordConfirmation: '123',
+        })
+        .expect(200);
+    });
   });
 });
