30e6f7de039131b111ddaf8f4e5ff8e132f3de98
Luan Panno <luanpanno@gmail.com>
2021-04-10 11:28:08 -0300

    feat: ensure AddSurveyController returns a badRequest error if validation fails

--- src/presentation/controllers/Survey/AddSurvey/AddSurveyController.spec.ts
+++ src/presentation/controllers/Survey/AddSurvey/AddSurveyController.spec.ts
@@ -2,6 +2,7 @@
 import { IHttpRequest } from '../../../protocols';
 import { IValidation } from '../../../protocols/IValidation';
 import { AddSurveyController } from './AddSurveyController';
+import { badRequest } from '../../../helpers/http/httpHelper';
 
 interface SutTypes {
   sut: AddSurveyController;
@@ -49,5 +50,15 @@ describe('AddSurvey Controller', () => {
     await sut.handle(httpRequest);
 
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body);
+  });
+
+  test('should return 400 if Validation fails', async () => {
+    const { sut, validationStub } = makeSut();
+
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error());
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+
+    expect(httpResponse).toEqual(badRequest(new Error()));
   });
 });
--- src/presentation/controllers/Survey/AddSurvey/AddSurveyController.ts
+++ src/presentation/controllers/Survey/AddSurvey/AddSurveyController.ts
@@ -1,3 +1,4 @@
+import { badRequest } from '../../../helpers';
 import { IController, IHttpRequest, IHttpResponse } from '../../../protocols';
 import { IValidation } from '../../../protocols/IValidation';
 
@@ -5,7 +6,11 @@ export class AddSurveyController implements IController {
   constructor(private readonly validation: IValidation) {}
 
   async handle(httpRequest: IHttpRequest): Promise<IHttpResponse> {
-    this.validation.validate(httpRequest.body);
+    const error = this.validation.validate(httpRequest.body);
+
+    if (error) {
+      return badRequest(error);
+    }
 
     return Promise.resolve(null);
   }
