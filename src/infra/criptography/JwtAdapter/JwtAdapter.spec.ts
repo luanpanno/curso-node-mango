6419553d4bf4466265859f5014da57cbdf361c4e
Luan Panno <luanpanno@gmail.com>
2021-04-13 00:07:49 -0300

    test: ensure JwtAdapter throws if verify throws

--- src/infra/criptography/JwtAdapter/JwtAdapter.spec.ts
+++ src/infra/criptography/JwtAdapter/JwtAdapter.spec.ts
@@ -70,5 +70,17 @@ describe('JWT Adapter', () => {
 
       expect(value).toBe('any_value');
     });
+
+    test('should throw if verify throws', async () => {
+      const { sut } = makeSut();
+
+      jest.spyOn(jwt, 'verify').mockImplementationOnce(() => {
+        throw new Error();
+      });
+
+      const promise = sut.decrypt('any_token');
+
+      await expect(promise).rejects.toThrow();
+    });
   });
 });
