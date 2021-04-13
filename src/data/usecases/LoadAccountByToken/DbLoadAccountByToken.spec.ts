ae44d3acf6088f67a0e7db3450d360849a63963d
Luan Panno <luanpanno@gmail.com>
2021-04-12 21:58:32 -0300

    test: ensure DbLoadAccountByToken returns null if LoadAccountByTokenRepository returns null

--- src/data/usecases/LoadAccountByToken/DbLoadAccountByToken.spec.ts
+++ src/data/usecases/LoadAccountByToken/DbLoadAccountByToken.spec.ts
@@ -86,4 +86,16 @@ describe('DbLoadAccountByToken usecase', () => {
 
     expect(loadByTokenSpy).toHaveBeenCalledWith('any_token', 'any_role');
   });
+
+  test('should return null if LoadAccountByTokenRepository returns null', async () => {
+    const { sut, loadAccountByTokenRepositoryStub } = makeSut();
+
+    jest
+      .spyOn(loadAccountByTokenRepositoryStub, 'loadByToken')
+      .mockReturnValueOnce(Promise.resolve(null));
+
+    const account = await sut.load('any_token', 'any_role');
+
+    expect(account).toBeNull();
+  });
 });
