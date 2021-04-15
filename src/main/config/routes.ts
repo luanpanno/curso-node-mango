c690b4b9a7994dfa951a9d2ab5e829a5be99b1a2
Luan Panno <luanpanno@gmail.com>
2021-04-14 22:56:21 -0300

    fix: make controllers return the error message instead of error name

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -8,7 +8,7 @@ export default (app: Express): void => {
 
   // eslint-disable-next-line node/no-path-concat
   readdirSync(`${__dirname}/../routes`).map(async (file) => {
-    if (!file.includes('.test.')) {
+    if (!file.includes('.test.') && !file.endsWith('.map')) {
       (await import(`../routes/${file}`)).default(router);
     }
   });
