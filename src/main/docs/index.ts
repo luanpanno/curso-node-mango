5832867929ca8f20c65a37a3300e2c690267cc03
Luan Panno <luanpanno@gmail.com>
2021-04-27 22:33:44 -0300

    feat: login routes swagger

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,3 +1,7 @@
+import { loginPath } from './paths/loginPath';
+import { accountSchema } from './schemas/accountSchema';
+import { loginParamsSchema } from './schemas/loginParamsSchema';
+
 export default {
   openapi: '3.0.0',
   info: {
@@ -5,5 +9,22 @@ export default {
     description:
       'API do curso do Mango para realizar enquetes entre programadores',
     version: '1.0.0',
+  },
+  servers: [
+    {
+      url: '/api/v1',
+    },
+  ],
+  tags: [
+    {
+      name: 'Login',
+    },
+  ],
+  paths: {
+    '/login': loginPath,
+  },
+  schemas: {
+    account: accountSchema,
+    'login-params': loginParamsSchema,
   },
 };
