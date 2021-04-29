6a3d2dd58d150f545748b99b65cb0f0db19afc86
Luan Panno <luanpanno@gmail.com>
2021-04-28 22:24:42 -0300

    docs: add load survey api swagger

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,11 +1,17 @@
 import { badRequest } from './components/badRequest';
+import { forbidden } from './components/forbidden';
 import { notFound } from './components/notFound';
 import { serverError } from './components/serverError';
 import { unauthorized } from './components/unauthorized';
 import { loginPath } from './paths/loginPath';
+import { surveyPath } from './paths/surveyPath';
 import { accountSchema } from './schemas/accountSchema';
+import { apiKeyAuthSchema } from './schemas/apiKeyAuthSchema';
 import { errorSchema } from './schemas/errorSchema';
 import { loginParamsSchema } from './schemas/loginParamsSchema';
+import { surveyAnswerSchema } from './schemas/surveyAnswerSchema';
+import { surveySchema } from './schemas/surveySchema';
+import { surveysSchema } from './schemas/surveysSchema';
 
 export default {
   openapi: '3.0.0',
@@ -24,19 +30,30 @@ export default {
     {
       name: 'Login',
     },
+    {
+      name: 'Enquetes',
+    },
   ],
   paths: {
     '/login': loginPath,
+    '/surveys': surveyPath,
   },
   schemas: {
     account: accountSchema,
     'login-params': loginParamsSchema,
     error: errorSchema,
+    surveys: surveysSchema,
+    survey: surveySchema,
+    surveyAnswer: surveyAnswerSchema,
   },
   components: {
+    securitySchemes: {
+      apiKeyAuth: apiKeyAuthSchema,
+    },
     badRequest,
     serverError,
     unauthorized,
     notFound,
+    forbidden,
   },
 };
