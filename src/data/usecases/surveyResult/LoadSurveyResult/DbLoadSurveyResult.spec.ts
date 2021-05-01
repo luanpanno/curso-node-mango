10e59f5b1542208132c2a799aa7100ab5cc0b4f6
Luan Panno <luanpanno@gmail.com>
2021-05-01 00:47:54 -0300

    test: ensure DbLoadSurveyResult throws if LoadSurveyResultRepository throws

--- src/data/usecases/surveyResult/LoadSurveyResult/DbLoadSurveyResult.spec.ts
+++ src/data/usecases/surveyResult/LoadSurveyResult/DbLoadSurveyResult.spec.ts
@@ -27,4 +27,15 @@ describe('DbLoadSurveyResult UseCase', () => {
 
     expect(loadBySurveyIdSpy).toHaveBeenCalledWith('any_survey_id');
   });
+
+  test('Should throw if LoadSurveyResultRepository throws', async () => {
+    const { sut, loadSurveyResultRepositoryStub } = makeSut();
+    jest
+      .spyOn(loadSurveyResultRepositoryStub, 'loadBySurveyId')
+      .mockReturnValueOnce(Promise.reject(new Error()));
+
+    const promise = sut.load('any_survey_id');
+
+    await expect(promise).rejects.toThrow();
+  });
 });
