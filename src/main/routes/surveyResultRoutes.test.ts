1ee31f0f5f749c51137857437b7e228f2bb9364e
Luan Panno <luanpanno@gmail.com>
2021-05-02 02:17:16 -0300

    test: ensure load survey route returns 200 on success

--- src/main/routes/surveyResultRoutes.test.ts
+++ src/main/routes/surveyResultRoutes.test.ts
@@ -91,5 +91,28 @@ describe('Survey Routes', () => {
     test('should return status 403 on load survey result without accessToken', async () => {
       await request(app).get('/api/v1/surveys/any_id/results').expect(403);
     });
+
+    test('should return status 200 on load survey result with accessToken', async () => {
+      const accessToken = await makeAccessToken();
+      const res = await surveyCollection.insertOne({
+        question: 'Question',
+        answers: [
+          {
+            answer: 'Answer 1',
+            image: '',
+          },
+          {
+            answer: 'Answer 2',
+            image: '',
+          },
+        ],
+        date: new Date(),
+      });
+
+      await request(app)
+        .get(`/api/v1/surveys/${String(res.ops[0]._id)}/results`)
+        .set('x-access-token', accessToken)
+        .expect(200);
+    });
   });
 });
