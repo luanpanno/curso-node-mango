9741e2b352b5bc1aeb14633adcd34f01654f6018
Luan Panno <luanpanno@gmail.com>
2021-05-02 01:55:38 -0300

    feat: ensure LoadSurveyResultController returns 500 if LoadSurveyById throws

--- src/presentation/controllers/SurveyResult/LoadSurveyResult/LoadSurveyResultController.spec.ts
+++ src/presentation/controllers/SurveyResult/LoadSurveyResult/LoadSurveyResultController.spec.ts
@@ -1,6 +1,6 @@
 import { LoadSurveyById } from '@/domain/usecases/survey/LoadSurveyById';
 import { InvalidParamError } from '@/presentation/errors';
-import { forbidden } from '@/presentation/helpers';
+import { forbidden, serverError } from '@/presentation/helpers';
 import { HttpRequest } from '@/presentation/protocols';
 import { mockLoadSurveyById } from '@/presentation/test/mockSurvey';
 
@@ -47,5 +47,17 @@ describe('LoadSurveyResultController', () => {
     const httpResponse = await sut.handle(makeFakeRequest());
 
     expect(httpResponse).toEqual(forbidden(new InvalidParamError('surveyId')));
+  });
+
+  test('Should return 500 if LoadSurveyById throws', async () => {
+    const { sut, loadSurveyByIdStub } = makeSut();
+
+    jest
+      .spyOn(loadSurveyByIdStub, 'loadById')
+      .mockReturnValueOnce(Promise.reject(new Error()));
+
+    const httpResponse = await sut.handle(makeFakeRequest());
+
+    expect(httpResponse).toEqual(serverError(new Error()));
   });
 });
